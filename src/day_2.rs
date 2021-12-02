830bcd5635bbf93c719c9f7a997dd78ebdaf6b97
wmclt <william.mauclet+github@gmail.com>
2021-12-02 20:24:53 +0100

    halfway through move to util.rs

--- src/day_2.rs
+++ src/day_2.rs
@@ -6,25 +6,15 @@ pub fn part_1() -> i32 {
     count(instructions)
 }
 
-pub fn part_2() -> u32 {
-    666
-    // let file_path = "./data/day_1.txt";
-    // let numbers = load_numbers(file_path);
-    // let mut window_numbers = sum_windows(numbers);
-    // count_increases(&mut window_numbers)
+pub fn part_2() -> i32 {
+    let file_path = "./data/day_2.txt";
+    let instructions = load_instructions(file_path);
+    count_with_aim(instructions)
 }
 
-// fn sum_windows(numbers: Vec<i32>) -> Vec<i32> {
-//     let mut window_sums = Vec::new();
-//     for i in 0..numbers.len() - 2 {
-//         window_sums.push(numbers[i] + numbers[i + 1] + numbers[i + 2]);
-//     }
-//     window_sums
-// }
-
 fn count(movements: Vec<(String, i32)>) -> i32 {
-    let mut horizontal:i32 = 0;
-    let mut vertical:i32 = 0;
+    let mut horizontal: i32 = 0;
+    let mut vertical: i32 = 0;
 
     for (dir, nbr) in movements {
         match dir.as_str() {
@@ -41,20 +31,49 @@ fn count(movements: Vec<(String, i32)>) -> i32 {
         }
     }
 
-    (horizontal * vertical).into()
+    horizontal * vertical
+}
+
+fn count_with_aim(movements: Vec<(String, i32)>) -> i32 {
+    let mut aim: i32 = 0;
+    let mut horizontal: i32 = 0;
+    let mut depth: i32 = 0;
+
+    for (dir, nbr) in movements {
+        match dir.as_str() {
+            "forward" => {
+                horizontal += nbr;
+                depth += aim * nbr;
+            }
+            "down" => {
+                aim += nbr;
+            }
+            "up" => {
+                aim -= nbr;
+            }
+            _ => {}
+        }
+    }
+
+    horizontal * depth
 }
 
 fn load_instructions(filename: &str) -> Vec<(String, i32)> {
     let mut direction_moves: Vec<(String, i32)> = Vec::new();
     if let Ok(lines) = crate::util::read_lines(filename) {
         for line in lines.flatten() {
-            let mut split_string: Vec<&str> = line.split(" ").collect();
-            let nbr: i32 = FromStr::from_str(split_string.pop().unwrap()).unwrap();
-            let direction = split_string.pop().unwrap().to_string();
+            let (nbr, direction) = line_to_movement(line);
             direction_moves.push((direction, nbr));
         }
     }
     direction_moves
+}
+
+fn line_to_movement(line: String) -> (i32, String) {
+    let mut split_string: Vec<&str> = line.split(' ').collect();
+    let nbr: i32 = FromStr::from_str(split_string.pop().unwrap()).unwrap();
+    let direction = split_string.pop().unwrap().to_string();
+    (nbr, direction)
 }
 
 #[cfg(test)]
@@ -73,22 +92,4 @@ mod tests {
         ];
         assert_eq!(count(movements), 90);
     }
-
-    // #[test]
-    // fn other_test_part_1() {
-    //     let mut numbers = vec![32, 21, 25, 226, 321, 488, 531];
-    //     assert_eq!(count_increases(&mut numbers), 5);
-    // }
-
-    // #[test]
-    // fn test_sum_windows() {
-    //     let numbers = vec![1, 2, 3, 4, 6, 4, 4, 4];
-    //     assert_eq!(sum_windows(numbers), vec![6, 9, 13, 14, 14, 12]);
-    // }
-
-    // #[test]
-    // fn test_part_2() {
-    //     let numbers = vec![1, 2, 3, 4, 6, 4, 4, 4];
-    //     assert_eq!(count_increases(&mut sum_windows(numbers)), 3);
-    // }
 }
--- src/util.rs
+++ src/util.rs
@@ -9,3 +9,16 @@ where
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
+
+pub fn load<T, F>(filename: &str, test: F) -> Vec<T>
+where
+    F: Fn(String) -> T,
+{
+    let mut parsed_lines: Vec<T> = Vec::new();
+    if let Ok(lines) = crate::util::read_lines(filename) {
+        for line in lines.flatten() {
+            parsed_lines.push(test(line));
+        }
+    }
+    parsed_lines
+}
